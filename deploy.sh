d3c586d0bd875f3fddd63ce2e690f114561a624b
bkotak <bhavinkotak07@gmail.com>
2020-05-08 18:09:28 +0530

    Admin module added

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #setup local VM Kubernetes cluster
 minikube start --memory=4096
+sleep 2
 #Use minikube docker environment
 eval $(minikube docker-env)
 #Mount volume
-minikube mount /media/bhavin/New\ Volume1/mtech/sem4/project/vagrant/:/host
-
+minikube mount /media/bhavin/New\ Volume1/mtech/sem4/project/vagrant/machine_translation:/host &
+sleep 3
 #Deploy model
 kubectl apply -f mt-svc-deploy.yaml
 #Expose ports outside container
 kubectl expose deployment mt2 --type=LoadBalancer --port=8080
 #Create an external IP
-minikube service mt2
+#minikube service mt2 &
+
 
 #Deploy UI
 kubectl apply -f pyflask-svc-deploy.yaml
--- flask_project/flask_project/app.py
+++ flask_project/flask_project/app.py
@@ -21,21 +21,83 @@ if url == None:
 url += "/api/translate/"
 print(url)
 
+url_mapper = {}
+counter = 0
 
+UPLOAD_FOLDER="uploads/"
+
+model_status = {}
+
+def deploy_model(model_name):
+    out = subprocess.Popen(['bash','deploy_model.sh',model_name],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    res = str(stdout )
+    return res
 app = Flask(__name__)
 
+@app.route('/deploy/<model>')
+def deploy(model):
+    res = deploy_model(model)
+    model_status[model] = 'DEPLOYED'   
+
     
+    return res
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        from_language = request.form['from_language']
+        to_language = request.form['to_language']
+        global counter
+        key = from_language+":"+to_language
+        val = 'model' + str(counter)
+        url_mapper[key] = val
+        counter += 1
+        f = request.files['file']
+        f.save(UPLOAD_FOLDER + val + ".zip")
+        model_status[key] = 'UPLOADED'  
+        return 'Uploaded successfully'
+    else:
+        #return "admin1"
+        return render_template('admin.html')  
+@app.route('/list_models')
+def list_models():
+    return str(url_mapper)
+@app.route('/models/<model_name>')
+def check_deployment(model_name):
+    reply = requests.get('http://' + model_name + ":8080/dir")  
+    return reply.text
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/process', methods=['POST','GET'])
-def process():
+@app.route('/process_test', methods=['POST','GET'])
+def process_test():
     from_language = request.form['from_language']
     to_language = request.form['to_language']
     input_val = request.form['input_val']
 
-    request_reply = requests.get(url + input_val)
+    data = { 'paragraph':input_val }
+    print('Data:',data)
+    request_reply = requests.post(url=url, data=data)
+    #request_reply = requests.get(url + input_val)
+    output_val = request_reply.text
+    print(output_val)
+    return jsonify({'output_val' : output_val})
+
+@app.route('/process', methods=['POST','GET'])
+def process():
+    global counter
+    from_language = request.form['from_language']
+    to_language = request.form['to_language']
+    input_val = request.form['input_val']
+    key = from_language + ":" + to_language
+    if key not in url_mapper:
+        return 'Not found'
+    data = { 'paragraph':input_val }
+    print('Data:',data)
+    end_point = "http://" + url_mapper[key] + ":8080/api/translate/"
+    #return str(end_point)
+    request_reply = requests.post(url=end_point, data=data)
+    #request_reply = requests.get(url + input_val)
     output_val = request_reply.text
     print(output_val)
 
